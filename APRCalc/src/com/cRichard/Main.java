036041b1fd22442a13231e4943b997494943f009
drawsome1 <rchum@ucsd.edu>
2021-03-21 23:01:24 -0700

    saving projects

---
+++ APRCalc/src/com/cRichard/Main.java
@@ -0,0 +1,151 @@
+package com.cRichard;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+
+
+// write your code here
+public class Main {
+
+    private static double interest = 0d;
+
+    public static void main(String[] args) {
+        double startingPrincipal = 8800;
+        int year = 2020;
+        int month = 5; //June
+        int date = 1; //arbitrary date
+        double apr = 17.99;
+        int payment = 332;
+
+        double currentBalance = startingPrincipal;
+        System.out.println("Starting Balance = " + currentBalance);
+
+        int iterator = 0;
+        while (currentBalance > 0)
+        {
+//            //first few months will pay 800
+//            if (iterator >= 0 && iterator <= 3){
+//               payment = 700;
+//            }
+//            else if (iterator < 18){
+//                payment = 400;
+//            }
+//            //will start to pay 600 after 4 payments
+//            else {
+//                payment = 300;
+//            }
+            System.out.println("==== iteration " + iterator + "=================");
+            System.out.println("Current balace = " + currentBalance);
+            currentBalance = calcMonthlyBalance(currentBalance,year,month,date,apr,payment,iterator);
+
+            if (month == 11){
+                month = 0;
+                year++;
+            }
+            else {
+                month++;
+            }
+            iterator++;
+
+            if (currentBalance > startingPrincipal){
+                System.out.println("Nah Fuck This, You gotta pay more hommie");
+                System.out.println("Total interest = " + interest);
+                break;
+            }
+            else if (currentBalance == 0){
+                System.out.println("paid off in " + iterator/12 + " years and "
+                                    + iterator%12 + " months");
+            }
+        }
+
+    }
+
+    //simulating making a payment
+
+    public static double makePayment(double Principal, double payment,int month, int iterator){
+        if (Principal - payment <= 0){
+            payment = Principal;
+        }
+
+        System.out.println("Making a payment of: " + payment);
+
+//        if (month == 10){
+//            if (iterator == 5){
+//                payment += 2000;
+//                System.out.println("Paid 2000 more from annual bonus");
+//            }
+//            else if (iterator == 17){
+//                payment += 0;
+//            }
+//            else {
+//                payment += 1000;
+//                System.out.println("Paid 1000 more from annual bonus");
+//            }
+//        }
+
+
+       return Principal - payment;
+    }
+
+    public static int GetDaysInMonth(int year, int month, int date){
+        // create a calendar object
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(year,month,date);
+
+
+        return calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+    }
+
+    public static double InterestAdded(double principal, double APR, int numDays){
+        double daysInYear = 365.00d;
+
+        if(APR > 100 || APR < 0){
+            System.out.println("Invalid APR");
+            return 0f;
+        }
+
+        double aprPercent = (double)APR / (double)100.00d;
+
+        System.out.println("APR Percent = " + aprPercent);
+
+        double interest = (principal * (aprPercent/daysInYear) * numDays);
+
+        return round(interest);
+    }
+
+    public static double round(double amount){
+        return Math.round(amount * 100.00d)/ 100.00d;
+    }
+
+    public static double calcMonthlyBalance(double Principal, int year, int month, int date, double apr, int payment, int iterator){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year,month,date);
+
+
+        double preInterest = makePayment(Principal,payment,month,iterator);
+        System.out.println("balance before interest charge = " + preInterest);
+
+
+        int numDays = GetDaysInMonth(year,month,date);
+        System.out.println("Number of Days = " + numDays);
+
+        double InterestThisMonth = InterestAdded(preInterest,apr,numDays);
+        System.out.println("Interest added for the month of "
+                            + (new SimpleDateFormat("MMM").format(calendar.getTime()))
+                            + " = " + InterestThisMonth);
+
+        interest += InterestThisMonth;
+        System.out.println("Interest accrued: " + round(interest));
+
+
+
+        double newTotal = preInterest + InterestThisMonth;
+        System.out.println("New Total = " + round(newTotal));
+
+        System.out.println("=======================================");
+
+        return round(newTotal);
+    }
+}
